5b82804ef4863f7e36ae8891ca379165f6b3e2df
dfort <dforteza@cifpfbmoll.eu>
2021-02-16 18:59:27 +0100

    All pages

--- src/main/java/com/fbmoll/ad/demo/Scraper.java
+++ src/main/java/com/fbmoll/ad/demo/Scraper.java
@@ -4,6 +4,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import javax.lang.model.util.Elements;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URLEncoder;
@@ -12,21 +13,21 @@ import java.util.List;
 
 public class Scraper {
     //URL: https://www.google.com/search?q=
-    //Selector: h3.r a
+    //Selector: .yuRUbf a[data-ved][ping]
+
     public static final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36";
 
     public List<SearchResult> searchResults() throws IOException {
         List<SearchResult> resultList = new ArrayList<>();
-        final String query = "españa";
+        final String query = "macarrones";
         final Document page = Jsoup.connect("https://www.google.com/search?q=" + URLEncoder.encode(query, "UTF-8")).userAgent(USER_AGENT).get();
 
 
-        for (Element searchResult : page.select("h3.r a")) {
-
-            String title = searchResult.text();
+        for (Element searchResult : page.select(".yuRUbf a[data-ved][ping]")) {
             String url = searchResult.attr("href");
+            String title = searchResult.text();
 
-            resultList.add(new SearchResult(title,url));
+            resultList.add(new SearchResult(url,title));
         }
         return resultList;
     }
--- src/test/java/com/fbmoll/ad/demo/BadgeApplicationTests.java
+++ src/test/java/com/fbmoll/ad/demo/BadgeApplicationTests.java
@@ -1,7 +1,15 @@
 package com.fbmoll.ad.demo;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class BadgeApplicationTests {
@@ -11,3 +19,4 @@ class BadgeApplicationTests {
 	}
 
 }
+
